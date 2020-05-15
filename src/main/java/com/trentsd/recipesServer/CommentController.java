3c66de70de3897bdb1ebf56564f8aa387204a7ca
Sean Trent <sean.d.trent@gmail.com>
2020-05-15 13:25:27 -0400

    Added CORS

--- src/main/java/com/trentsd/recipesServer/CommentController.java
+++ src/main/java/com/trentsd/recipesServer/CommentController.java
@@ -1,5 +1,6 @@
 package com.trentsd.recipesServer;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ public class CommentController {
     }
 
     @GetMapping(path = "page/{pageId}")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection<Comment> pageComments(@PathVariable String pageId) {
         return repository.findAll().stream()
             .filter(c -> c.getPageid().toString().equals(pageId))
--- src/main/java/com/trentsd/recipesServer/CommentRepository.java
+++ src/main/java/com/trentsd/recipesServer/CommentRepository.java
@@ -2,7 +2,9 @@ package com.trentsd.recipesServer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource
+@CrossOrigin(origins = "http://localhost:4200")
 interface CommentRepository extends JpaRepository<Comment, Long> { 
 }
